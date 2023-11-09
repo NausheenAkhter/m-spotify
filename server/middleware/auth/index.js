f4686bcb6cbd462ac7bae0430b1e45d4d3b306f6
Nausheen Akhter <nausheenakhter@Nausheens-MacBook-Pro.local>
2023-11-09 12:42:26 +0530

    removed logs

--- server/middleware/auth/index.js
+++ server/middleware/auth/index.js
@@ -1,19 +1,14 @@
 import Jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
-    console.log(req.headers, 'req');
     let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
     if (token && token.startsWith('Bearer ')) {
-      // Remove Bearer from string
-      token = token.slice(7, token.length);
+        token = token.slice(7, token.length);
     }
-    console.log(token, 'token here error');
-
-    if(!token) throw new Error('Access denied')
+    if (!token) throw new Error('Access denied')
     Jwt.verify(token, process.env.JWT_PRIVATE_KEY, (error, validToken) => {
-        if(error)  {
-            console.log(error, 'here error');
-            throw new Error('Access denied')}
+        if (error)
+            throw new Error('Access denied')
         req.user = validToken
         next()
     })
